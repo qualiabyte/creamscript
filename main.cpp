8199938b71ce3d6004f3ed72221f14f0c2bb1001
Tyler Florez <qualiabyte@gmail.com>
2014-11-02 22:16:34 -0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,60 @@
+
+#include <cassert>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+
+enum TokenType {
+    SPACE,          // SP
+    ALPHA,          // A-Z a-z
+    DIGIT,          // 0-9
+    PAREN_LEFT,     // (
+    PAREN_RIGHT,    // )
+    HYPHEN,         // -
+    UNDERSCORE,     // _
+    SIGN_EQ,        // =
+    SIGN_LT,        // <
+    SIGN_GT,        // >
+    QUOTE_DOUBLE,   // "
+    QUOTE_SINGLE    // '
+};
+
+struct Token {
+    int type;
+    char* value;
+};
+
+Token* tokenize(string s)
+{
+    int len = s.length();
+    Token* tokens = new Token[len];
+    int tokenCount = 0;
+    char c;
+    for (int i = 0; i < len; i++)
+    {
+        c = s[i];
+        char* value = new char[4];
+        sprintf(value, "%c", c);
+        if (isalpha(c))
+            tokens[tokenCount++] = { ALPHA, value };
+    }
+    return tokens;
+}
+
+void testTokenize()
+{
+  cout << "Testing tokenize" << endl;
+  auto tokens = tokenize("ABC");
+  assert(tokens[0].type == ALPHA);
+  assert(strcmp(tokens[0].value, "A") == 0);
+}
+
+int main()
+{
+    cout << "Running tests" << endl;
+    testTokenize();
+    cout << "Done!" << endl;
+    return 0;
+}
